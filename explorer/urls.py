4f7eeee6ad203b36913f230666dcb2adddc29f89
Marc Altmann <marcaltmann@posteo.de>
2026-05-13 22:32:29 +0200

    Register core app and wire up welcome view

--- explorer/urls.py
+++ explorer/urls.py
@@ -2,9 +2,12 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
+import explorer.core.views as core_views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
+    path('', core_views.welcome, name='welcome'),
 ]
 
 if settings.DJANGO_ENV == 'development':
